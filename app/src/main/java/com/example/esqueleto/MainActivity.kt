ebabd4a05c52af42b5d9e9394eeaa8440d174fa0
Gabo29val <ga_al5@hotmail.com>
2022-01-11 14:09:14 -0500

    estaa

--- app/src/main/java/com/example/esqueleto/MainActivity.kt
+++ app/src/main/java/com/example/esqueleto/MainActivity.kt
@@ -15,5 +15,7 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "old school", Toast.LENGTH_SHORT).show()
 
         Toast.makeText(this, "F", Toast.LENGTH_SHORT).show()
+
+        Toast.makeText(this, "a ver si es de tu talla", Toast.LENGTH_SHORT).show()
     }
 }
